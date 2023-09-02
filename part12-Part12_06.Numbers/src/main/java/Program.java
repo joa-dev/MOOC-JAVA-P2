1e844e49a24fa1485ec3c0c239b436dd8fdc4ff2
unknown <joa.neis.dev@gmail.com>
2023-09-02 07:41:38 -0300

    part12_06 completed

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,14 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    }
+        Random randomizer = new Random();
 
+        System.out.println("How many random numbers should be printed");
+
+        int input = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < input; i++) {
+            System.out.println(randomizer.nextInt(11));
+        }
+    }
 }
