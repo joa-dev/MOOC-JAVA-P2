db27e65ad4d42cc20273aaf379182b52c0bdbf6f
unknown <joa.neis.dev@gmail.com>
2023-09-06 22:44:42 -0300

    part13_08 completed

--- part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,10 @@
 package title;
 
+import javafx.application.Application;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Application.launch(UserTitle.class);
     }
-
 }
--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,18 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
+public class UserTitle extends Application {
 
-
+    @Override
+    public void start(Stage window) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+        //it didn't work with Parameters, but without it, it works(?)
+        window.setTitle(input);
+        window.show();
+    }
 }
