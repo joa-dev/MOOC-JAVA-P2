8881e8fec55103b0406edb3adff537141692de7c
unknown <joa.neis.dev@gmail.com>
2023-09-11 21:17:43 -0300

    part14_02 wip

--- part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -1,6 +1,30 @@
 package application;
 
-public class PartiesApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
+
+public class PartiesApplication extends Application {
+    @Override
+    public void start(Stage stage) {
+        NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
+        NumberAxis yAxis = new NumberAxis(0, 30, 5);
+        
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        
+        
+        
+        
+        
+        //.....................
+        Scene view = new Scene(lineChart, 640, 480);
+        stage.setScene(view);
+        stage.show();
+        
+        
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
