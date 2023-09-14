b99a6dd3cc09d4d917638006ada9a93900b76c13
unknown <joa.neis.dev@gmail.com>
2023-09-14 07:52:11 -0300

    part14_02 wip

--- part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -8,29 +13,50 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class PartiesApplication extends Application {
+
     @Override
     public void start(Stage stage) {
         NumberAxis xAxis = new NumberAxis(1968, 2008, 4);
         NumberAxis yAxis = new NumberAxis(0, 30, 5);
-        
+
         LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
-        
+
         XYChart.Series test = new XYChart.Series();
-        
-        
-        
-        
-        
+
+        //getting info from file
+        List<String> rows = new ArrayList<>();
+
+        try {
+            Files.lines(Paths.get("partiesdata.tsv")).forEach(row -> rows.add(row));
+            rows.forEach(row -> row.split("\t"));
+            System.out.println(rows);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
         //.....................
         Scene view = new Scene(lineChart, 640, 480);
         stage.setScene(view);
         stage.show();
-        
-        
+
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        List<String> rows = new ArrayList<>();
 
+        try {
+            String texto = Files.lines(Paths.get("partiesdata.tsv")).toString();
+            
+            
+            /*List<String> pieces = Arrays.asList(row.split("\t"));
+            List<String> party = Arrays.asList(rows.forEach(row -> row.split("\t")));
+            rows.forEach(row -> party.add(row.split("\t")));
+            for (int i = 0; i < party.size(); i++) {
+                System.out.println(i + ": " + party.get(i));
+            }*/
+            System.out.println(texto);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
